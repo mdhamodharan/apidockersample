f27c2c83c221bf66a9ef3f1fde3d18fd5ba969e7
mdhamodharan <37323700+mdhamodharan@users.noreply.github.com>
2021-06-11 13:32:32 +0530

    Initial

---
+++ ApiDockerSample1/Controllers/UserController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiDockerSample1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        public UserController()
+        {
+
+        }
+
+        [HttpGet]
+        [Route("getusers")]
+        public IActionResult GetAllUsers()
+        {
+            string[] userList = new string[2];
+            userList[0] = "User One";
+            userList[1] = "User Two";
+            return Ok(userList);
+        }
+
+    }
+}
